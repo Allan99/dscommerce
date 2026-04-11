f4cd2327b74b2ec03be0061f6c804f6242abb7f3
Allan <allanpereira773@gmail.com>
2026-04-11 15:36:27 -0300

    Put Product

--- src/main/java/com/devsuperior/dscommerce/servicies/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/servicies/ProductService.java
@@ -34,13 +34,24 @@ public class ProductService {
     @Transactional
     public ProductDTO insert(ProductDTO dto){
         Product product = new Product();
-        product.setName(dto.getName());
-        product.setId(dto.getId());
-        product.setDescription(dto.getDescription());
-        product.setPrice(dto.getPrice());
-        product.setImgUrl(dto.getImgUrl());
-
+        copyDtoToEntity(dto, product);
         product = repository.save(product);
         return new ProductDTO(product);
     }
+
+    @Transactional
+    public ProductDTO update(Long id, ProductDTO dto){
+        Product product = repository.getReferenceById(id);
+        copyDtoToEntity(dto, product);
+        product = repository.save(product);
+        return new ProductDTO(product);
+    }
+
+    public void copyDtoToEntity(ProductDTO dto, Product product){
+        product.setName(dto.getName());
+        product.setDescription(dto.getDescription());
+        product.setPrice(dto.getPrice());
+        product.setImgUrl(dto.getImgUrl());
+    }
+
 }
